27015b27bec19cae63bd43caf346e8caca119aba
zhang <zz593141477@gmail.com>
2018-06-30 10:07:11 +0800

    add DPY description

--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -83,10 +83,21 @@ module thinpad_top(
 
 /* =========== Demo code begin =========== */
 
+// 数码管连接关系示意图，dpy1同理
+// p=dpy0[0] // ---a---
+// c=dpy0[1] // |     |
+// d=dpy0[2] // f     b
+// e=dpy0[3] // |     |
+// b=dpy0[4] // ---g---
+// a=dpy0[5] // |     |
+// f=dpy0[6] // e     c
+// g=dpy0[7] // |     |
+//           // ---d---  p
+
 // 7段数码管译码器演示，将number用16进制显示在数码管上面
 reg[7:0] number;
-SEG7_LUT segL(.oSEG1(dpy0), .iDIG(number[3:0]));
-SEG7_LUT segH(.oSEG1(dpy1), .iDIG(number[7:4]));
+SEG7_LUT segL(.oSEG1(dpy0), .iDIG(number[3:0])); //dpy0是低位数码管
+SEG7_LUT segH(.oSEG1(dpy1), .iDIG(number[7:4])); //dpy1是高位数码管
 
 reg[15:0] led_bits;
 assign leds = led_bits;
