5f948110faccd2aff47c75b3d171a73024e878ca
张宇翔 <2017210832@admin.secoder.net>
2018-09-29 02:56:17 +0000

    Update build.tcl

--- build.tcl
+++ build.tcl
@@ -1,5 +1,21 @@
 update_compile_order -fileset sources_1
-#upgrade_ip [get_ips]
+
+# If IP cores are used
+if { [llength [get_ips]] != 0} {
+    upgrade_ip [get_ips]
+
+    set ip_runs {}
+    set run_suffix _synth_1
+    foreach ip [get_ips] {
+        create_ip_run [get_ips $ip]
+        lappend ip_runs $ip$run_suffix
+    }
+    launch_runs -quiet -jobs 2 {*}$ip_runs
+
+    foreach r $ip_runs {
+        wait_on_run $r
+    }
+}
 
 reset_run impl_1
 reset_run synth_1
