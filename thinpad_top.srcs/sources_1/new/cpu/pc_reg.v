2578fbbbca63ad9cfdf57cbdf9603da9d44d6500
翁家翌 <wengjy16@mails.tsinghua.edu.cn>
2018-10-30 21:58:18 +0800

    Rephase

--- thinpad_top.srcs/sources_1/new/cpu/pc_reg.v
+++ thinpad_top.srcs/sources_1/new/cpu/pc_reg.v
@@ -7,28 +7,38 @@ module pc_reg(
 
     input wire branch_flag_i,
     input wire[`RegBus] branch_target_address_i,
-    
+
+    input wire flush,
+    input wire[`RegBus] new_pc,
+
     output reg[`InstAddrBus] pc,
     output reg ce
 );
-
-    always @ (posedge clk) begin
-        if (ce == `ChipDisable) begin
-            pc <= `StartInstAddr;
-        end else if(stall[0] == `NoStop) begin
-            if(branch_flag_i == `Branch) begin
-                pc <= branch_target_address_i;
-            end else begin
-                pc <= pc + 4'h4;
-            end
-        end
-    end
 
     always @ (posedge clk) begin
         if (rst == `RstEnable) begin
             ce <= `ChipDisable;
         end else begin
             ce <= `ChipEnable;
+        end
+    end
+
+    reg is_rst;
+
+    always @ (posedge clk) begin
+        is_rst <= rst;
+        if (ce == `ChipDisable) begin
+            pc <= `StartInstAddr;
+        end else begin
+            if (is_rst == `RstEnable) begin
+                pc <= `StartInstAddr;
+            end else if(stall[0] == `NoStop) begin
+                if(branch_flag_i == `Branch) begin
+                    pc <= branch_target_address_i;
+                end else begin
+                    pc <= pc + 4'h4;
+                end
+            end
         end
     end
 
