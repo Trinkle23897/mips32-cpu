946976987a1d3ae8127ca962794bde32561512d5
Trinkle <463003665@qq.com>
2018-11-23 21:54:06 +0800

    fix critical warning and reach 0x5b

--- compile_cpu_func_test.sh
+++ compile_cpu_func_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd cpu_testcase/func_test/src
-make ver=sim
+make ver=sim clean
+make clean
+make ver=sim -j
 xxd -c 4 -g 4 -e main.bin | awk '{print $2}' > main.data
 cd ../../..
 cp cpu_testcase/func_test/src/main.data thinpad_top.srcs/sources_1/new
+cp cpu_testcase/func_test/src/main.bin thinpad_top.srcs/sources_1/new
